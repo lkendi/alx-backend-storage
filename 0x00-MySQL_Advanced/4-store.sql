3002adb8a9138e797e18c6cf9e44f7ddd9384c9b
lkendi <lkendi003@gmail.com>
2024-08-08 17:50:09 +0300

    Edit

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,5 @@
 --script that creates a trigger that decreases the qty
 --of an item after adding a new order
-DELIMITER @@
 
 CREATE TRIGGER decrease_stock
 AFTER INSERT ON orders
@@ -8,5 +7,4 @@ FOR EACH ROW
 BEGIN
     UPDATE items SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
-END @@
-DELIMITER ;
+END;
